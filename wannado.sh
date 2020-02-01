f48c56e6880ed8130fea0e56018ef934379b0754
hitler <a85504@alunos.uminho.pt>
2020-02-01 18:55:16 +0000

    idk

--- wannado.sh
+++ wannado.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-if (! [[ -f ~/.todo ]]) && [[ $1 == "add" ]]
+if (! [[ -f ~/.todo ]] ) && [[ $1 == "add" ]]
 then
 	echo 0:u:$2 > ~/.todo
 	echo TaskId: 0
@@ -22,17 +22,17 @@ else
 			sed -ri 's|(^'"$2"':)u|\1c|g' ~/.todo
 			;;
 		todo)
-			sed -r "/^.*:c/d" ~/.todo | awk -F: '{print $3}'
+			sed -r "/^.*:c:/d" ~/.todo | awk -F: '{print $3}'
 			;;
 		done)
-			sed -r "/^.*:u/d" ~/.todo | awk -F: '{print $3}'
+			sed -r "/^.*:u:/d" ~/.todo | awk -F: '{print $3}'
 			;;
 		rm)
 			sed -ri "/^$2:/d" ~/.todo
 			;;
-		all)
+		*)
 			echo -e "$GREEN"Done: $NC
-			sed -r "/^.*:u/d" ~/.todo | awk -F: '{print "\t"$1": "$3}'
+		    sed -r "/^.*:u/d" ~/.todo | awk -F: '{print "\t"$1": "$3}'
 			echo -e "$RED"ToDo: $NC
 			sed -r "/^.*:c/d" ~/.todo | awk -F: '{print "\t"$1": "$3}'
 			;;
